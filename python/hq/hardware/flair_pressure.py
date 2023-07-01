b4a7fd8def93109246e99531c606f7f70f434e56
Hamish Morgan <ham430@gmail.com>
2023-07-01 05:38:41 +0100

    further improvements to flair pressure robustness

--- python/hq/hardware/flair_pressure.py
+++ python/hq/hardware/flair_pressure.py
@@ -8,13 +8,14 @@ Read pressure from flair and provide a timer
 
 
 import os
+import signal
 import sys
 from math import atan2, degrees
-from subprocess import run
+from pathlib import Path
 from random import shuffle
+from subprocess import run
 from time import sleep, time
 from typing import Optional
-from pathlib import Path
 
 import cv2
 import numpy as np
@@ -23,36 +24,54 @@ from hq.io_tools import getchar
 CTRL_C = b"\x03"
 BACKSPACE = b"\x7f"
 CLEAR = b"\x0c"
-CURSOR_HOME = "\033[H"
+CURSOR_HOME = "\033"
 SPACE = b" "
 
-DIAL_Y_MIN = 214
-DIAL_Y_MAX = 307
-DIAL_X_MIN = 209
-DIAL_X_MAX = 403
+DIAL_Y_MIN = 208
+DIAL_Y_MAX = 300
+DIAL_X_MIN = 212
+DIAL_X_MAX = 404
 
 ZERO_ANGLE = 189
 NINE_BAR_ANGLE = 40
 DEGREES_PER_BAR = 8
 
-GRAPH_ROWS = 20
+GRAPH_ROWS = 12
 GRAPH_COLS = 60
 
 
+def signal_handler(sig, frame):
+    sys.stdout.write("\n" * 2)
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, signal_handler)
+
+
 def clear() -> None:
+    """
+    Clear screen
+    """
     run("clear", shell=True)
 
 
 class FlairPressure:
+    """
+    Flair 58 pressure gauge reader and TUI
+    """
+    
     def __init__(self) -> None:
         """
         Construct the Flair pressure reader
         """
         self.load_dial_mask()
-        self.needle_cetre = np.array((267 - DIAL_Y_MIN, 312 - DIAL_X_MIN))
+        self.needle_cetre = np.array((254 - DIAL_Y_MIN, 316 - DIAL_X_MIN))
+        self.log_to_stderr = "LOG_STDERR" in os.environ
+        self.debug_mode = "FP_DEBUG" in os.environ
         self.pressure_graph = np.full((GRAPH_ROWS + 2, GRAPH_COLS + 2), fill_value="\0")
         self.pressure_graph[:, 0] = " "
-        self.pressure_graph[1:GRAPH_ROWS:2, 0] = np.arange(9, -1, -1)
+        self.pressure_graph[1:GRAPH_ROWS, 0] = np.arange(GRAPH_ROWS - 2, -1, -1)
+        self.pressure_graph[1, 0] = "X"
         self.pressure_graph[:, 1] = "|"
         self.pressure_graph[GRAPH_ROWS, :] = "-"
 
@@ -61,7 +80,8 @@ class FlairPressure:
         Capture an image from the webcam
 
         Args:
-            vid_in: Existing video capture object. New one crated if not given
+            vid_in: Existing video capture object. New one created if not given
+
         Returns:
             Image from webcam as (H, W, 3) array
         """
@@ -70,9 +90,6 @@ class FlairPressure:
             vid = cv2.VideoCapture(0)
         else:
             vid = vid_in
-
-        # # Set capture parameters
-        # cap.set(cv2.cv.CV_CAP_PROP_EXPOSURE, 0.1)
 
         # Capture the frame
         _, frame = vid.read()
@@ -121,94 +138,58 @@ class FlairPressure:
             return None
 
         # Convert image to grayscale
-        # gray = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)
         gray = img[..., 2]  # red channel should minimise red writing visibility
 
+        # Crop image down to just the dial
         cropped = gray[DIAL_Y_MIN:DIAL_Y_MAX, DIAL_X_MIN:DIAL_X_MAX, ...]
 
-        # # Decrease contrast
-        # alpha = 0.5
-        # beta = 0
-        # gray = np.clip(gray * alpha + beta, 0, 255).astype(np.uint8)
-
+        # Do histogram equalisation to make needle darkness consistent
         equ = cv2.equalizeHist(cropped)
 
         # Blur image to optimise thresholding performance
-        # blur = cv2.GaussianBlur(src=gray, ksize=(13, 13), sigmaX=0)
         blur = cv2.GaussianBlur(src=equ, ksize=(5, 5), sigmaX=0)
 
-        # canny = cv2.Canny(image=equ, threshold1=30, threshold2=100)
-        # canny[self.dial_mask < 255] = 0
-
-        # lines = cv2.HoughLines(
-        #     canny,  # image
-        #     1,  # rho
-        #     np.pi / 360,  # theta
-        #     5,  # threshold
-        #     None,  # srn
-        #     0,  # stn
-        #     0,  # min_theta
-        # )
-
-        # if lines is not None:
-        #     return np.mean([line[0][1] for line in lines])
-        # return None
-
-        cv2.imwrite(filename="/tmp/gray.png", img=gray)
-        cv2.imwrite(filename="/tmp/contrast.png", img=gray)
-        cv2.imwrite(filename="/tmp/blur.png", img=blur)
-        cv2.imwrite(filename="/tmp/equ.png", img=equ)
-        # cv2.imwrite(filename="/tmp/canny.png", img=canny)
-
-        # # Find centre of needle tail
-        # aw = np.argwhere(canny)
-        # if aw.size == 0:
-        #     return None
-        # centroid = np.mean(aw, axis=0).astype(int)
-        # centroid = tuple(reversed(centroid))  # Switch to X, Y instead of Y, X
-
-        # # clahe = cv2.createCLAHE(clipLimit=5)
-        # # equalised = clahe.apply(blur)
-
         # Binarise by thresholding
-        # _, binary = cv2.threshold(src=blur, thresh=0, maxval=255, type=cv2.THRESH_OTSU)
         _, binary = cv2.threshold(
             src=blur, thresh=80, maxval=255, type=cv2.THRESH_BINARY
         )
-        # binary = cv2.adaptiveThreshold(
-        #     # blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 7
-        #     equ, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 25, 11
-        # )
 
         # Mask out everything but the dial
         binary[self.dial_mask == 0] = 255
 
-        # Erode then dilate to remove noise
+        # Dilate to remove noise
         dilated = cv2.dilate(binary, kernel=np.ones((5, 5)), iterations=1)
 
         # Find centre of needle tail
         centroid = np.mean(np.argwhere(255 - dilated), axis=0).astype(int)
         centroid = tuple(reversed(centroid))  # Switch to X, Y instead of Y, X
 
-        # Get angle of needle from centre spindle
+        # Get angle of needle relative to centre spindle
         direction_vector = centroid - self.needle_cetre
         angle = degrees(atan2(*direction_vector))
         if angle < 0:
             angle = 360 + angle
 
-        # Apply some calibration function to produce a pressure value
+        # Apply a transformation function to produce a pressure value
+        # TODO: nonlinear function
         pressure = max(0, (ZERO_ANGLE - angle) / NINE_BAR_ANGLE * DEGREES_PER_BAR)
 
-        cv2.imwrite(
-            filename="/tmp/centroid.png",
-            img=cv2.line(dilated, centroid, centroid, (255, 0, 0), 2),
-        )
-        cv2.imwrite(filename="/tmp/binary_masked.png", img=binary)
-        cv2.imwrite(filename="/tmp/dilated.png", img=dilated)
-        cv2.imwrite(
-            filename="/tmp/centroid.png",
-            img=cv2.line(dilated, centroid, centroid, (255, 0, 0), 2),
-        )
+        if self.debug_mode:
+            cv2.imwrite(filename="/tmp/gray.png", img=gray)
+            cv2.imwrite(filename="/tmp/contrast.png", img=gray)
+            cv2.imwrite(filename="/tmp/blur.png", img=blur)
+            cv2.imwrite(filename="/tmp/equ.png", img=equ)
+            cv2.imwrite(
+                filename="/tmp/centroid.png",
+                img=cv2.line(dilated, centroid, centroid, (255, 0, 0), 2),
+            )
+            cv2.imwrite(filename="/tmp/binary_masked.png", img=binary)
+            cv2.imwrite(filename="/tmp/dilated.png", img=dilated)
+            cv2.imwrite(
+                filename="/tmp/centroid.png",
+                img=cv2.line(dilated, centroid, centroid, (255, 0, 0), 2),
+            )
+            breakpoint()
 
         return pressure
 
@@ -229,7 +210,7 @@ class FlairPressure:
         self.pressure_graph[-1, 3:] = self.pressure_graph[-1, 2:-1]
         self.pressure_graph[-1, 3] = " "
         if new_time is not None:
-            self.pressure_graph[-1, 3:5] = list(f"{new_time:2.0f}")
+            self.pressure_graph[-1, 3:6] = list(f"{new_time:3.0f}")
 
         # Construct string representation
         return "\n".join(
@@ -249,61 +230,77 @@ class FlairPressure:
         """
         Main CLI tool routine
         """
+        # Open video capture object
         vid = cv2.VideoCapture(0)
-        log_directory = "/tmp/flair_pressure_logs"
-        os.makedirs(log_directory, exist_ok=True)
 
+        # Clear screen
         clear()
+
+        # Initialise loop variables
         start = time()
+        i = 0
+        pressures = []
+        last_capture_time = -1.0
+        last_redraw_time = -1.0
+        updates_per_refresh = 50
+        num_updates = 0
 
-        with open(
-            os.path.join(log_directory, "pressures.log"), mode="w", encoding="utf-8"
-        ) as log_fp:
-            i = 0
-            pressures = []
-            last_capture_time_int = -1
-            while True:
-                output_path = os.path.join(log_directory, f"{i}.jpg")
+        while True:
 
-                # Capture and save image
-                img = self.capture_frame(vid_in=vid)
-                capture_time = time() - start
-                # cv2.imwrite(filename=output_path, img=img)
+            # Capture image
+            img = self.capture_frame(vid_in=vid)
+            capture_time = time() - start
 
-                # Compute angle of needle
-                pressure = self.detect_needle_position(img=img)
-                if pressure is not None:
-                    pressures.append(pressure)
+            # Compute pressure from angle of needle
+            pressure = self.detect_needle_position(img=img)
+            if pressure is not None:
+                pressures.append(pressure)
 
-                capture_time_int = int(capture_time)
-                if capture_time_int != last_capture_time_int:
-                    avg_pressure = np.mean(pressures) if pressures else 0
-                    pressure_graph = self.update_pressures(
-                        new_pressure=avg_pressure,
-                        new_time=capture_time if capture_time_int % 5 == 0 else None,
-                    )
-                    sys.stdout.write(CURSOR_HOME + "\n" * 2 + pressure_graph + "\n" * 2)
-                    pressures = []
-                    last_capture_time_int = capture_time_int
-
-                pressure_str = f"{pressure:.2f}" if pressure is not None else "N/A"
-                sys.stdout.write(
-                    f"\r  Time: {capture_time:.2f}, Pressure: {pressure_str} bar"
+            # Redraw graph periodically
+            if capture_time > last_capture_time + 0.5:
+                avg_pressure = np.mean(pressures) if pressures else 0
+                if num_updates >= updates_per_refresh:
+                    num_updates = 0
+                    graph_time = capture_time
+                else:
+                    graph_time = None
+                pressure_graph = self.update_pressures(
+                    new_pressure=avg_pressure,
+                    new_time=graph_time,
                 )
-                log_fp.write(f"{pressure}\n")
+                pressures = []
+                last_capture_time = capture_time
+                if capture_time > last_redraw_time + 1.5:
+                    clear()
+                    sys.stdout.write(pressure_graph + "\n" * 2)
+                    last_redraw_time = capture_time
 
-                i += 1
+            # Print fast-refresh pressure and time status line
+            pressure_str = f"{pressure:.2f}" if pressure is not None else "N/A"
+            sys.stdout.write(
+                f"\r\tTime: {capture_time:.2f}\tPressure: {pressure_str} bar"
+            )
+            if self.log_to_stderr:
+                sys.stderr.write(f"{pressure}\n")
+
+            # Update loop variables
+            i += 1
+            num_updates += 1
 
         vid.release()
 
     def load_dial_mask(self) -> None:
-        # mask_path = os.path.expanduser("~/src/hq/etc/flair_pressure/dial_mask.png")
+        """
+        Load segmentation mask of relevant portion of flair dial
+        """
         mask_path = os.path.expanduser(
-            "~/src/hq/etc/flair_pressure/dial_mask_cropped.png"
+            "~/src/hq/etc/flair_pressure/dial_mask.png"
         )
-        mask = cv2.imread(mask_path)
+        mask = cv2.imread(mask_path)[DIAL_Y_MIN:DIAL_Y_MAX, DIAL_X_MIN:DIAL_X_MAX, ...]
         if mask is None:
             raise ValueError(f"Could not load mask at {mask_path}")
+
+        # Mask is saved as RGB, just take first channel
         self.dial_mask = mask[..., 0]
 
 
