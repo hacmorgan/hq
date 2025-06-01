fc74e7116266e983cc325713a8534bed21064ba6
Hamish Morgan <ham430@gmail.com>
2025-06-01 16:07:23 +1000

    Anniversary 2025 card added to UI

--- hq/gui/dashboard/lib/main.dart
+++ hq/gui/dashboard/lib/main.dart
@@ -1,132 +1,3 @@
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         // This is the theme of your application.
-//         //
-//         // TRY THIS: Try running your application with "flutter run". You'll see
-//         // the application has a purple toolbar. Then, without quitting the app,
-//         // try changing the seedColor in the colorScheme below to Colors.green
-//         // and then invoke "hot reload" (save your changes or press the "hot
-//         // reload" button in a Flutter-supported IDE, or press "r" if you used
-//         // the command line to start the app).
-//         //
-//         // Notice that the counter didn't reset back to zero; the application
-//         // state is not lost during the reload. To reset the state, use hot
-//         // restart instead.
-//         //
-//         // This works for code too, not just values: Most code changes can be
-//         // tested with just a hot reload.
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-//     );
-//   }
-// }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-
-//   // This widget is the home page of your application. It is stateful, meaning
-//   // that it has a State object (defined below) that contains fields that affect
-//   // how it looks.
-
-//   // This class is the configuration for the state. It holds the values (in this
-//   // case the title) provided by the parent (in this case the App widget) and
-//   // used by the build method of the State. Fields in a Widget subclass are
-//   // always marked "final".
-
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       // This call to setState tells the Flutter framework that something has
-//       // changed in this State, which causes it to rerun the build method below
-//       // so that the display can reflect the updated values. If we changed
-//       // _counter without calling setState(), then the build method would not be
-//       // called again, and so nothing would appear to happen.
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // This method is rerun every time setState is called, for instance as done
-//     // by the _incrementCounter method above.
-//     //
-//     // The Flutter framework has been optimized to make rerunning build methods
-//     // fast, so that you can just rebuild anything that needs updating rather
-//     // than having to individually change instances of widgets.
-//     return Scaffold(
-//       appBar: AppBar(
-//         // TRY THIS: Try changing the color here to a specific color (to
-//         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-//         // change color while the other colors stay the same.
-//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-//         // Here we take the value from the MyHomePage object that was created by
-//         // the App.build method, and use it to set our appbar title.
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         // Center is a layout widget. It takes a single child and positions it
-//         // in the middle of the parent.
-//         child: Column(
-//           // Column is also a layout widget. It takes a list of children and
-//           // arranges them vertically. By default, it sizes itself to fit its
-//           // children horizontally, and tries to be as tall as its parent.
-//           //
-//           // Column has various properties to control how it sizes itself and
-//           // how it positions its children. Here we use mainAxisAlignment to
-//           // center the children vertically; the main axis here is the vertical
-//           // axis because Columns are vertical (the cross axis would be
-//           // horizontal).
-//           //
-//           // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-//           // action in the IDE, or press "p" in the console), to see the
-//           // wireframe for each widget.
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headlineMedium,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
-
-
-// // //
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -324,7 +195,7 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  void _showBirthdayDialog() {
+  void _show2025BirthdayDialog() {
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -387,6 +258,80 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  void _show2025AnniversaryDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Happy Anniversary! 💍'),
+          content: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(
+                  'anniversary-2025.jpg',
+                  height: 500,
+                  errorBuilder: (context, error, stackTrace) =>
+                    const Icon(Icons.cake, size: 100, color: Colors.pink),
+                ),
+                const SizedBox(height: 16),
+                const Text(
+                  'Happy 8 years mi amor ❤️',
+                  style: TextStyle(fontSize: 18),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 16),
+                const Text(
+                  "I don't like thinking about my life without you, because I don't like being sad, but for the sake of this card I did, and the simulation was bleak and depressing 😞",
+                 style: TextStyle(fontSize: 14),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 16),
+                const Text(
+                  "You have brought so many incredible things to my life over the years, and it continues to this day. On one end of the spectrum, things as mundane as my organisational and time management skills have improved where they never really had before; while on the other, we have a legit family together and I feel like a real dad. Then there's all my newfound knowledge of medical and animal things which make people think I'm cool and relatable, there's all the culinary lessons you've given me, the support and opportunity to do my silly projects and waste time seasoning my pans, all the random experiments we do together, the free reign over the workshop, the time and space for my excessive coffee routines, the delicious brown onion steak sauce (and all other dishes too he just deserves a shoutout), and of course, Babos",
+                  style: TextStyle(fontSize: 14),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 15),
+                const Text(
+                  "You're my best friend in the world, and my motivation for pretty much everything. Actually that puts a lot on you; really it's that I don't need to work to motivate myself to do stuff when you're around. I want to make and eat good food with you, I want to go out for walks and exercise with you, I want to cuddle up and watch movies with you, and I just want to spend all my time with you; and when you're gone it all just falls away. Honestly that song \"Ain't no sunshine (when she's gone)\" is bang on",
+                  style: TextStyle(fontSize: 14),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 16),
+                const Text(
+                  "You're my whole thing, and I can't wait to see where we end up together. Nobody could ever come close to you, I wish I could let you inside my brain and show you because everyone else is dumb bitches I tell you hwat, I can't believe you're real",
+                  style: TextStyle(fontSize: 14),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 16),
+                const Text(
+                  "Be mine forever?",
+                  style: TextStyle(fontSize: 14),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 16),
+                const Text(
+                  'Love you ∞ + 1',
+                  style: TextStyle(fontSize: 14),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('okay let\'s wrap this up'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -408,9 +353,9 @@ class _MyHomePageState extends State<MyHomePage> {
           _widgetOptions()[_selectedIndex],
           Positioned(
             right: 16,
-            bottom: 16,
+            bottom: 88,
             child: GestureDetector(
-              onTap: _showBirthdayDialog,
+              onTap: _show2025BirthdayDialog,
               child: Container(
                 width: 56,
                 height: 56,
@@ -428,6 +373,34 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 child: const Icon(
                   Icons.cake,
+                  color: Colors.pink,
+                  size: 30,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            right: 16,
+            bottom: 16,
+            child: GestureDetector(
+              onTap: _show2025AnniversaryDialog,
+              child: Container(
+                width: 56,
+                height: 56,
+                decoration: BoxDecoration(
+                  color: Colors.cyan[100],
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      spreadRadius: 1,
+                      blurRadius: 3,
+                      offset: const Offset(0, 2),
+                    ),
+                  ],
+                ),
+                child: const Icon(
+                  Icons.favorite,
                   color: Colors.pink,
                   size: 30,
                 ),
