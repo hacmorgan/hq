c6af2d63f3bacfb3f2b166086577c36b02e5e4e4
Hamish Morgan <ham430@gmail.com>
2025-07-01 09:54:07 +1000

    Add separate spice house and spice up aliases

--- examples/autokbd.py
+++ examples/autokbd.py
@@ -4,7 +4,7 @@
 """
 Record or execute keyboard macros
 
-Recording keystrokes (until hitting `esc`):
+Recording keystrokes (until hitting `space`):
 
     sudo examples/autokbd.py record > etc/keyboard-automation/harvest.cloudpickle
 
@@ -20,10 +20,14 @@ from time import sleep
 import sys
 
 
+# Record mode: read keystrokes until spacebar is pressed, and dump cloudpickled macro to
+# stdout
 if len(sys.argv) > 1 and sys.argv[1] in "record":
     recorded = keyboard.record(until="space")
     cloudpickle.dump(recorded, sys.stdout.buffer)
+
+# Playback mode: load cloudpickled macro from stdin and playback keystrokes at 4x speed
 else:
     recorded = cloudpickle.load(sys.stdin.buffer)
-    sleep(2.0)
-    keyboard.play(recorded, speed_factor=5)
+    sleep(2.0)  # Give a moment to position cursor on the relevant window
+    keyboard.play(recorded, speed_factor=4)
